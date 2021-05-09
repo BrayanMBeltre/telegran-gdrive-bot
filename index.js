426568d43b75fb7ce07efd63f6fb178716bd99ab
Brayan MBeltre <brayanmb07@gmail.com>
2021-05-09 00:37:18 -0700

    FIXED document not showing  FIXED upload file folder destination  FIXED craete folders name cache  FIXED open folder in browser

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // TODO Divide in bot{commands, stages, index}, gdrive
 require("dotenv").config();
+
 const {
   Telegraf,
   Markup,
   session,
   Scenes: { BaseScene, Stage, WizardScene },
 } = require("telegraf");
+
 const {
   createFolder,
   getFileFromTelegram,
@@ -31,20 +33,30 @@ const cancelButton = () => {
 
 // SCENES
 
-// FIXME pdf not showing
-// FIXME craete folders name cache
-// FIXME upload file without file in it
+// FIXED document not showing
+// FIXED upload file folder destination
+// FIXED craete folders name cache
+// FIXED open folder in browser
 // FIXME auto rerun
 // FIXME add a back button when entering a folder
 
 // NEW LIST FOLDERS SCENE
 const listFoldersScene = new BaseScene("listFoldersScene")
+
+listFoldersScene.leave(ctx => {
+  console.log("leaving")
+})
+
 listFoldersScene.enter(async (ctx) => {
   ctx.session.folders = [];
   ctx.session.folderId = "";
   ctx.session.token = "";
   ctx.session.index = 0;
   ctx.session.deepCount = 0;
+  ctx.session.folderName = "";
+  ctx.session.photos = [];
+  ctx.session.documents = [];
+  ctx.session.others = [];
 
   // feedback
   ctx.telegram.sendChatAction(ctx.chat.id, "typing");
@@ -63,35 +75,38 @@ listFoldersScene.enter(async (ctx) => {
 
   const showFiles = async (folderId) => {
     const { files } = await listFiles(folderId)
-    // console.log(files)
-    const photos = [];
-    const pdfs = [];
-    const others = [];
+    let photos = [];
+    let documents = [];
+    let others = [];
 
+    // files.map(file => {
+    //   file.mimeType == "image/jpeg" ? photos.push(file) :
+    //     file.mimeType == "application/document" ? documents.push(file) :
+    //       others.push(file)
+    // })
     files.map(file => {
-      file.mimeType == "image/jpeg" ? photos.push(file) :
-        file.mimeType == "application/pdf" ? pdfs.push(file) :
-          others.push(file)
+      file.mimeType == "image/jpeg" ? ctx.session.photos.push(file) :
+        file.mimeType == "application/pdf" ? ctx.session.documents.push(file) :
+          ctx.session.others.push(file)
     })
 
-    const hidePhotosButton = (photos.length <= 0)
-    const hidePdfsButton = (pdfs.length <= 0)
-    const hideOthersButton = (others.length <= 0)
+    const hidePhotosButton = (ctx.session.photos.length <= 0)
+    const hideDocumentsButton = (ctx.session.documents.length <= 0)
+    const hideOthersButton = (ctx.session.others.length <= 0)
 
     await ctx.reply("Select", Markup.inlineKeyboard(
       [
         [
-          Markup.button.callback(`Show photos: ${photos.length}`, "photo", hidePhotosButton),
+          Markup.button.callback(`Show photos: ${ctx.session.photos.length}`, "photo", hidePhotosButton),
         ],
         [
-          Markup.button.callback(`Show PDFs: ${pdfs.length}`, "pdf", hidePdfsButton),
+          Markup.button.callback(`Show documents: ${ctx.session.documents.length}`, "document", hideDocumentsButton),
         ],
         [
-          Markup.button.callback(`Show others: ${others.length}`, "other", hideOthersButton),
+          Markup.button.callback(`Show others: ${ctx.session.others.length}`, "other", hideOthersButton),
         ],
         [
-          Markup.button.callback(`Upload files in this folder`, "upload", hideOthersButton),
-
+          Markup.button.callback(`Upload files in this folder`, "upload"),
         ],
         [
           Markup.button.callback("Cancel", "exit")
@@ -100,39 +115,41 @@ listFoldersScene.enter(async (ctx) => {
     ))
 
     bot.action("photo", (ctx) => {
+      // feedback
       ctx.telegram.sendChatAction(ctx.chat.id, "typing");
       ctx.deleteMessage()
       ctx.replyWithMediaGroup([
-
-        ...photos.map(photo => (
+        ...ctx.session.photos.map(photo => (
           {
             "media": photo.webContentLink,
-            "caption": photo.name,
+            "caption": "Photos",
             "type": "photo"
 
           }))
-
       ])
+
     });
 
-    bot.action("pdf", (ctx) => {
+    bot.action("document", (ctx) => {
+      // feedback
       ctx.telegram.sendChatAction(ctx.chat.id, "typing");
       ctx.deleteMessage()
       ctx.replyWithMediaGroup([
-        ...pdfs.map(pdf => (
+        ...ctx.session.documents.map(document => (
           {
-            "media": pdf.webContentLink,
-            "caption": pdf.name,
+            "media": document.webContentLink,
+            "caption": "Documents",
             "type": "document"
 
           }))
       ])
+
     });
 
     bot.action("other", async (ctx) => {
       ctx.telegram.sendChatAction(ctx.chat.id, "typing");
       ctx.deleteMessage()
-      const { webViewLink } = await generatePublicUrl(folderId);
+      const { webViewLink } = await generatePublicUrl(ctx.session.folderId);
       ctx.reply("Not available", inlineUrlKeyboard("Open in browser", webViewLink))
     })
 
@@ -140,7 +157,7 @@ listFoldersScene.enter(async (ctx) => {
       ctx.deleteMessage()
       // feedback
       ctx.telegram.sendChatAction(ctx.chat.id, "typing");
-      ctx.reply("Select PDFs or photos as a file")
+      ctx.reply("Select documents or photos as a file")
 
       listFoldersScene.on("document", async (ctx) => {
         // feedback
@@ -150,7 +167,7 @@ listFoldersScene.enter(async (ctx) => {
         try {
           const { href } = await ctx.telegram.getFileLink(file_id);
           const data = await getFileFromTelegram(href);
-          const response = await uploadFile(file_name, mime_type, data, folderId);
+          const response = await uploadFile(file_name, mime_type, data, ctx.session.folderId);
           const { webViewLink, webContentLink } = await generatePublicUrl(response.id);
 
           ctx.reply(`${response.name} has been uploaded!`);
@@ -170,6 +187,8 @@ listFoldersScene.enter(async (ctx) => {
       });
 
     });
+
+    return ctx.scene.leave();
   }
 
   const newKeyboard = (files, size) => {
@@ -187,6 +206,10 @@ listFoldersScene.enter(async (ctx) => {
           ctx.deleteMessage();
           ctx.session.folders = [];
           ctx.session.index = 0;
+          ctx.session.folderId = "";
+          ctx.session.photos = [];
+          ctx.session.documents = [];
+          ctx.session.others = [];
           await getFiles(file.id)
           ctx.session.folderId = file.id;
           ctx.session.deepCount == 0 ? callMe() : showFiles(file.id);
@@ -215,28 +238,29 @@ listFoldersScene.enter(async (ctx) => {
       ctx.telegram.sendChatAction(ctx.chat.id, "typing");
       ctx.reply("Type folder name")
       listFoldersScene.on("text", async (ctx) => {
+        ctx.session.folderName = ctx.message.text;
+        // feedback
+        ctx.telegram.sendChatAction(ctx.chat.id, "typing");
+        ctx.reply(`Folder name is ${ctx.session.folderName}?`,
+          Markup.inlineKeyboard(
+            [
+              Markup.button.callback("Cancel", "exit"),
+              Markup.button.callback("Confirm", "newFolder")
+            ]))
 
         try {
-          const folder_name = ctx.message.text;
-          // feedback
-          ctx.telegram.sendChatAction(ctx.chat.id, "typing");
-          ctx.reply(`Folder name is ${folder_name}?`,
-            Markup.inlineKeyboard(
-              [
-                Markup.button.callback("Cancel", "exit"),
-                Markup.button.callback("Confirm", "newFolder")
-              ]))
-
           bot.action("newFolder", async (ctx) => {
             ctx.deleteMessage()
             // feedback
             ctx.telegram.sendChatAction(ctx.chat.id, "upload_document");
 
-            const response = await createFolder(folder_name, ctx.session.folderId);
+            const response = await createFolder(ctx.session.folderName, ctx.session.folderId);
             const { webViewLink } = await generatePublicUrl(response.id);
 
             ctx.reply(`${response.name} has been created!`);
             inlineUrlKeyboard("Open", webViewLink);
+
+            return ctx.scene.leave();
           })
 
         } catch (error) {
@@ -295,10 +319,14 @@ bot.launch();
 //  ERRORS
 process.on("uncaughtException", function (error) {
   console.log("\x1b[31m", "Exception: ", error, "\x1b[0m");
+  process.exit(1);
+  console.log("restarted")
 });
 
 process.on("unhandledRejection", function (error, p) {
   console.log("\x1b[31m", "Error: ", error.message, "\x1b[0m");
+  process.exit(1);
+  console.log("restarted")
 });
 
 // Enable graceful stop
